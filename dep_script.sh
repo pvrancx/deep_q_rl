a8404dcc6c7b235e034a2dd23a44ce675532e169
Peter Vrancx <pvrancx@vub.ac.be>
2016-07-03 15:27:08 +0200

    updating dependencies

--- dep_script.sh
+++ dep_script.sh
@@ -11,12 +11,27 @@ sudo apt-get install python-opencv
 echo "==>installing Matplotlib..."
 sudo apt-get install python-matplotlib python-tk
 
+echo "==>installing mongodb ..."
+sudo apt-get install mongodb
+
+echo "==>installing pymongo ..."
+pip install --user --upgrade pymongo
+
+echo "==>installing openai gym ..."
+pip install --user gym[all]
+
 echo "==>installing Theano ..."
 # some dependencies ...
 sudo apt-get install python-numpy python-scipy python-dev python-pip python-nose g++ libopenblas-dev git
 pip install --user --upgrade --no-deps git+git://github.com/Theano/Theano.git
 
 echo "==>installing Lasagne ..."
+pip install --user --upgrade https://github.com/Lasagne/Lasagne/archive/master.zip
+
+echo "==>installing pymongo ..."
+pip install --user --upgrade https://github.com/Lasagne/Lasagne/archive/master.zip
+
+echo "==>installing Gym ..."
 pip install --user --upgrade https://github.com/Lasagne/Lasagne/archive/master.zip
 
 # Packages below this point require downloads. 
@@ -34,19 +49,6 @@ cd ./pylearn2
 python setup.py develop --user
 cd ..
 
-if [ ! -d "./ALE" ]
-then
-echo "==>installing ALE ..."
 
-# dependencies ...
-sudo apt-get install libsdl1.2-dev libsdl-gfx1.2-dev libsdl-image1.2-dev cmake
-
-git clone https://github.com/mgbellemare/Arcade-Learning-Environment ALE
-cd ./ALE
-cmake -DUSE_SDL=ON -DUSE_RLGLUE=OFF .
-make -j2
-pip install --user .
-cd ..
-fi
 
 echo "==>All done!"
